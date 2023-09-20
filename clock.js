d7264ee9ecf97e634a8f3bd2ec24af2166db9c19
Reza zhyani <116823598+imalirezai@users.noreply.github.com>
2023-09-20 11:10:29 +0330

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,28 @@
+const hourEl = document.getElementById("hour");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm");
+
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "Am";
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  if (h > 12) {
+    h = h - 12;
+    ampm = "Pm";
+  }
+  hourEl.innerText = h;
+  minutesEl.innerText = m;
+  secondsEl.innerText = s;
+  ampm, (innerText = ampm);
+  setInterval(() => {
+    updateClock();
+  }, 1000);
+}
+updateClock();
